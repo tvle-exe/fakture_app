4f66f1f066f6c7a3b9c9acd019ef3b0042ce0d3e
tvle-exe <mc.antonio.donut@gmail.com>
2026-02-04 17:40:47 +0100

    new button

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -3,6 +3,9 @@ from views.main_window import MainWindow
 from controllers.invoice_controller import InvoiceController
 from controllers.clients_controller import ClientsController
 from controllers.history_controller import HistoryController
+import os
+import sys
+import subprocess
 
 
 class MainController:
@@ -16,6 +19,7 @@ class MainController:
         self.window.ui.novaFakturaButton.clicked.connect(self.open_invoice)
         self.window.ui.clientsButton.clicked.connect(self.open_clients)
         self.window.ui.povijestFakturaButton.clicked.connect(self.open_history)
+        self.window.ui.openPdfFolderButton.clicked.connect(self.open_pdf_folder)
 
     def open_invoice(self):
         self.invoice_controller = InvoiceController()
@@ -28,3 +32,19 @@ class MainController:
     def open_history(self): 
         self.history_controller = HistoryController()
         self.history_controller.open()
+
+    def open_pdf_folder(self):
+        # putanja do pdf foldera u rootu
+        if getattr(sys, 'frozen', False):
+            base_dir = os.path.dirname(sys.executable)
+        else:
+            base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        pdf_dir = os.path.join(base_dir, "pdf")
+        os.makedirs(pdf_dir, exist_ok=True)
+
+        if sys.platform == "win32":
+            os.startfile(pdf_dir)
+        elif sys.platform == "darwin":
+            subprocess.run(["open", pdf_dir])
+        else:  # linux
+            subprocess.run(["xdg-open", pdf_dir])
